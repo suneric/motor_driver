292bd2b6bbfdd10acc4bd346155f6516d096dc11
suner <syf_more@163.com>
2022-04-20 12:48:14 -0400

    fix a typo in path

--- auto_start.sh
+++ auto_start.sh
@@ -5,5 +5,5 @@ export ROS_IP=192.168.1.15
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
 
-python3 /home/ubuntu/catkin_ws/src/motor_diver/scripts/motor_driver_interface.py
+python3 /home/ubuntu/catkin_ws/src/motor_driver/scripts/motor_driver_interface.py
 aplay /home/ubuntu/catkin_ws/src/motor_driver/sound/heart_beat.wav

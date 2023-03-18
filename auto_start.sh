6c5090c977380245f4ca194538cd3780778c05f0
suneric <syf_more@163.com>
2023-03-18 18:32:46 -0400

    correct auto start

--- auto_start.sh
+++ auto_start.sh
@@ -5,4 +5,4 @@ export ROS_MASTER_URI=http://192.168.1.7:11311
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
 
-python3 /home/ubuntu/catkin_ws/src/motor_driver/scripts/motor_driver_interface.py
+python3 /home/ubuntu/catkin_ws/src/motor_driver/scripts/servo_motor_driver_interface.py

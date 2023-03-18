9f3b9a039861124165cbf026be1a1734eba06aaf
suneric <syf_more@163.com>
2023-03-18 18:23:12 -0400

    change rosmaster setting

--- auto_start.sh
+++ auto_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ROS_HOSTNAME=ubuntu-desktop
-export ROS_MASTER_URI=http://ubuntu-Aurora-R7:11311
+export ROS_MASTER_URI=http://192.168.1.7:11311
 
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
